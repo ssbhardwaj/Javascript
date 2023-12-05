db6f271076b32b3ee7c0dccaedc3a83133efaeee
Suman Shekhar Bhardwaj <106861018+ssbhardwaj@users.noreply.github.com>
2023-12-05 18:43:10 +0000

    Comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);  //  convert string to int
+
+// console.log(null > 0)       // false
+// console.log(null == 0);     // false
+// console.log(null >= 0)      // true -> because pehle value m convert hota hain..
+// // Avoid these kind of comparisions
+
+// console.log(undefined > 0)      // false
+// console.log(undefined == 0)     // false
+// console.log(undefined < 0)      // false
+// Avoid these kind of comparisions
+
+
+// ===      =>  strict comparision. i.e checks value as well as data types
+console.log("2" === 2)      // false
