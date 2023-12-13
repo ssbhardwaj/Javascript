dde0082e91f3ee197c65e4e41a20c2165bda7ad4
Suman Shekhar Bhardwaj <106861018+ssbhardwaj@users.noreply.github.com>
2023-12-13 06:21:31 +0000

    F_M_R

---
+++ 05_iterations/filter_map_reduce.js
@@ -0,0 +1,103 @@
+// const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// const values = coding.forEach( (item) => {
+//     // console.log(item)
+//     return item
+// } )
+
+// console.log(values)
+
+// for each loop does not return any value..
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNums.filter( (num) => num > 4 )
+// const newNums = myNums.filter( (num) => {
+//     return num > 4
+// } )
+// console.log(newNums)
+
+// const newNums = []
+
+// myNums.forEach( (num) => {
+//     if(num > 4){
+//         newNums.push(num)
+//     }
+// } )
+// console.log(newNums)
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+const userBooks = books.filter( (bk) => {
+    return bk.genre === "History"
+} )
+
+// console.log(userBooks);
+
+
+// ******************************************* Map ***************************************************
+// map ek value return karta hain
+
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumbers.map( (num) => {
+//     return num + 10;
+// } )
+
+const newNums = myNumbers
+                .map( (num) => num*10)
+                .map( (num) => num+1 )
+                .filter( (num) => num >= 41 )
+
+// console.log(newNums)
+
+
+// ************************************** Reduce **************************************
+
+const myNum = [1, 2, 3]
+
+// const myTotal = myNum.reduce( function(acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`)
+//     return acc + currval
+// }, 0)
+
+const myTotal = myNum.reduce( (acc, currval) => {
+    return acc + currval
+}, 0)
+
+console.log(myTotal)
+
+const shoppingCart = [
+    {
+        itemName: "js Course",
+        price: 2999
+    },
+    {
+        itemName: "py Course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev Course",
+        price: 5999
+    },
+    {
+        itemName: "data science Course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce( (acc, item) => {
+    return acc + item.price
+}, 0)
+
+console.log(priceToPay)
